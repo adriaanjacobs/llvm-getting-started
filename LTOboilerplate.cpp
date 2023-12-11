93b53c3e1cd051e59942d1b1216e6ea0683dffa7
Adriaan Jacobs <adriaan.jacobs@kuleuven.be>
2023-12-11 17:43:48 +0100

    add out-of-tree LTO library

---
+++ LTOboilerplate.cpp
@@ -0,0 +1,27 @@
+#include "pass.h"
+
+#include <llvm/Passes/PassPlugin.h>
+#include <llvm/Passes/PassBuilder.h>
+
+// This part is the new way of registering your pass
+extern "C" ::llvm::PassPluginLibraryInfo LLVM_ATTRIBUTE_WEAK llvmGetPassPluginInfo() {
+    return {
+        LLVM_PLUGIN_API_VERSION, "AttestInstrumentLTO", LLVM_VERSION_STRING,
+        [](llvm::PassBuilder &PB) {
+            PB.registerPipelineParsingCallback([](llvm::StringRef name, llvm::ModulePassManager& MPM, llvm::ArrayRef<llvm::PassBuilder::PipelineElement>) -> bool {
+                if (name == "llvm-getting-started-pass") {
+                    MPM.addPass(MyInstrumentationPass{});
+                    return true;
+                } return false;
+            });
+            PB.registerFullLinkTimeOptimizationLastEPCallback([](llvm::ModulePassManager &MPM, llvm::OptimizationLevel Level) {
+                llvm::outs() << "Registering pass for LateLTO!\n";
+                MPM.addPass(MyInstrumentationPass{});
+            });
+            PB.registerAnalysisRegistrationCallback([] (llvm::ModuleAnalysisManager& MAM) {
+                MAM.registerPass([] () { return MyAnalysis{}; });
+            });
+        }
+    };
+}
+
